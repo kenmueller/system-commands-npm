cd308ce705ec1ef2168d4b30062aa8e78ad1aae6
Ken Mueller <kenmueller0@gmail.com>
2019-09-02 20:24:23 -0700

    Fixed typescript definition file

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,12 @@
-/**
- * Runs a system command
- * 
- * @param command The command you want to run, like `ls` or `mkdir new_directory`
- * 
- * @returns A `Promise` containing the output of the command. If the command failed, the error is passed into the `.catch` block.
- */
-export default function(command: string): Promise<string>
+declare module 'system-commands' {
+	/**
+	 * Runs a system command
+	 * 
+	 * @param command The command you want to run, like `ls` or `mkdir new_directory`
+	 * 
+	 * @returns A `Promise` containing the output of the command. If the command failed, the error is passed into the `.catch` block.
+	 */
+	function system(command: string): Promise<string>
+
+	export = system
+}
